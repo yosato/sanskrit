45b126dea233ffad4a2db63cd185937d14c3d63f
Yo Sato <yosato16@gmail.com>
2017-08-12 14:25:02 +0100

    paradigm file added

---
+++ paradigms.py
@@ -0,0 +1,371 @@
+
+Nominals={
+###===== pronouns ==============
+
+#yad/tad-replace ad wit:{
+'PronYadTadM':{
+'Nom':['ah', 'o'],[ 'au'], ['e',  'āni']
+'Voc':['']
+'Acc':['am'],['au'],['ān']
+'Inst':[ 'ena'], [ 'ābhyām'], ['aiḥ',  'air']
+'Dat':[ 'asmai'], [ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Abl':['asmāt',  'asmād',  'asmādd'],[ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Gen':[ 'asya'], ['ayoḥ',  'ayor'],['eṣām',  'eṣāṃ']
+'Loc':['asmin', 'asmim', 'asmiṃ'], ['ayoḥ', 'ayor'],['eṣu']
+}
+'PronYadTadF':{
+'Nom':['ā'],['e'], ['āḥ']
+'Voc':['']
+'Acc':['ām'],['e'],['āḥ']
+'Inst':[ 'ayā'], [ 'ābhyām'], ['ābhiḥ',  'ābhir']
+'Dat':[ 'syai'], [ 'ābhyām'], ['ābhyaḥ',  'ābhyo']
+'Abl':[ 'asyāḥ'],[ 'ābhyām'], ['ābhyaḥ',  'ābhyo']
+'Gen':[ 'asyāḥ'], ['ayoḥ',  'ayor'],['āṣām',  'āṣāṃ']
+'Loc':[ 'asyām'], ['ayoḥ', 'ayor'],['āsu']
+}
+'PronYadTadN':{
+'Nom':['ad', 'at'],['e'], [ 'āni']
+'Voc':['']
+'Acc':['ad', 'at'],['e'],['āni']
+'Inst':[ 'ena'], [ 'ābhyām'], ['aiḥ',  'air']
+'Dat':[ 'asmai'], [ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Abl':['asmāt',  'asmād',  'asmādd'],[ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Gen':[ 'asya'], ['ayoḥ',  'ayor'],['eṣām',  'eṣāṃ']
+'Loc':['asmin', 'asmim', 'asmiṃ'], ['ayoḥ', 'ayor'],['eṣu']
+}
+
+#idam replace the whole word idam with:{
+'PronIdamM':{
+'Nom':['ayam'], ['imau'], ['ime']
+'Voc':['']
+'Acc':['imam'],['imau'], ['imān']
+'Inst':['anena'], ['ābhyām'],['ebhiḥ', 'ebhir']
+'Dat':['asmai'],[, ['ābhyām'],['ebhyaḥ', 'ebhyo']
+'Abl':['asmāt'], ['ābhyām'],['ebhyaḥ', 'ebhyo']
+'Gen':['asya'], 'anayoḥ', 'anayor'], ['eṣām']
+'Loc':['asmin', 'asmim', 'asmiṃ'], ['anayoḥ', 'anayor'],['eṣu']
+}
+'PronIdamF':{
+'Nom':['iyam'],['ime'], ['imāḥ']
+'Voc':['']
+'Acc':['imām'],['ime'],['imāḥ']
+'Inst':[ 'anayā'], [ 'ābhyām'], ['ābhiḥ',  'ābhir']
+'Dat':[ 'asyai'], [ 'ābhyām'], ['ābhyaḥ',  'ābhyo']
+'Abl':[ 'asyāḥ'],[ 'ābhyām'], ['ābhyaḥ',  'ābhyo']
+'Gen':[ 'asyāḥ'], ['ayoḥ',  'ayor'],['āṣām',  'āṣāṃ']
+'Loc':[ 'asyām'], ['ayoḥ', 'ayor'],['āsu']
+}
+
+
+'PronIdamN':{
+
+'Nom':['idam'],['ime'], [ 'imāni']
+'Voc':['']
+'Acc':['idam'],['ime'],['imāni']
+'Inst':[ 'anena'], [ 'ābhyām'], ['aiḥ',  'air']
+'Dat':[ 'asmai'], [ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Abl':['asmāt',  'asmād',  'asmādd'],[ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Gen':[ 'asya'], ['ayoḥ',  'ayor'],['eṣām',  'eṣāṃ']
+'Loc':['asmin', 'asmim', 'asmiṃ'], ['ayoḥ', 'ayor'],['eṣu']
+}
+
+# for the words :'sarva', 'anya', 'para', 'katama', 'viśva', 'sva', 'eka', 'itara', 'antara', uttara. 'adhara', pūrva
+# replace final a 'with', for the pronoun 'kim', replace im with:
+'PronAorImM' :{
+
+'Nom':['a'], ['au'], ['e']
+'Voc':['a'], ['au'], ['e']
+'Acc':['am'],['au'], ['ān']
+'Inst':['ena'], ['ābhyām'],['ebhiḥ', 'ebhir']
+'Dat':['ai'],[, ['ābhyām'],['ebhyaḥ', 'ebhyo']
+'Abl':['āt'], ['ābhyām'],['ebhyaḥ', 'ebhyo']
+'Gen':['asya'], ['anayoḥ', 'anayor'], ['eṣām']
+'Loc':['im', 'asmin', 'asmim', 'asmiṃ'], ['anayoḥ', 'anayor'],['eṣu']
+}
+'PronAorImF':{
+'Nom':['ā'],['e'], ['āḥ']
+'Voc':['']
+'Acc':['ām'],['e'],['āḥ']
+'Inst':[ 'ayā'], [ 'ābhyām'], ['ābhiḥ',  'ābhir']
+'Dat':[ 'ayai'], [ 'ābhyām'], ['ābhyaḥ',  'ābhyo']
+'Abl':[ 'ayāḥ'],[ 'ābhyām'], ['ābhyaḥ',  'ābhyo']
+'Gen':[ 'ayāḥ'], ['ayoḥ',  'ayor'],['āṣām',  'āṣāṃ']
+'Loc':[ 'ayām'], ['ayoḥ', 'ayor'],['āsu']
+}
+
+
+'PronAorImN':{
+
+'Nom':['am'],['e'], [ 'āni']
+'Voc':['am'],['e'], [ 'āni']
+'Acc':['am'],['e'], [ 'āni']
+'Inst':[ 'ena'], [ 'ābhyām'], ['aiḥ',  'air']
+'Dat':[ 'ai'], [ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Abl':['āt',  'asmād',  'asmādd'],[ 'ābhyām'], ['ebhyaḥ',  'ebhyo']
+'Gen':[ 'asya'], ['ayoḥ',  'ayor'],['eṣām',  'eṣāṃ']
+'Loc':['im', 'asmin', 'asmim', 'asmiṃ'], ['ayoḥ', 'ayor'],['eṣu']
+}
+
+
+#For the pronoun 'adas', replace entire word with:
+'adas' :{
+
+'Nom':['asau'], ['amū'], ['amī']
+'Voc':['']
+'Acc':['amum'], ['amū'], ['amūn']
+'Inst':['amunā'], ['amūbhyām'],['amībhiḥ', 'amībhir']
+'Dat':['amuṣmai'], ['amūbhyām'],['amībhyaḥ', 'amībhyo']
+'Abl':['amuñmāt'], ['amūbhyām'],['amībhyaḥ', 'amībhyo']
+'Gen':['amuṣya'], ['amuyoḥ', 'amuyor'], ['amīṣām']
+'Loc':['amuṣsmiṃ'], ['amuyoḥ', 'amuyor'],['amīṣu']
+}
+
+#personal pronouns
+'PPron1p':{
+'Nom':['aham', 'ahaṃ'],['āvām', 'nau'],['vayam', 'nah', 'no']
+'Acc':['mām', 'mā'],['māvām', 'nau'],['asmān', 'asmāṃ', 'naḥ', 'no']
+'Inst':['mayā'],['āvābhyām'],['asmābhiḥ', 'asmābhir']
+'Dat':['me', 'mahyam', 'mahyaṃ'],['āvābhyām'],['naḥ', 'asmābhyām']
+'Abl':['mat', 'me'],['āvābhyām', 'nau'],['asmat', 'naḥ', 'no']
+'Gen':['mama', 'me'],['āvayoḥ', 'āvayor', 'nau'],['asmākam', 'naḥ', 'no']
+'loc':['mayi'],['āvayoḥ', 'āvayor'],['asmāsu']
+}
+
+'PPron2p':{
+'Nom':['tvam', 'tvaṃ'],['yuvām', 'vām'],['yūyam', 'vah', 'vo']
+'Acc':['tvām', 'tvā'],['yuvām', 'vām'],['yūṣmān', 'asmāṃ', 'vaḥ', 'vo']
+'Inst':['tvayā'],['yuvābhyām'],['yūṣmābhiḥ', 'asmābhir']
+'Dat':['te', 'tubhyam', 'tubhyaṃ'],['yuvābhyām'],['naḥ', 'yūṣmābhyām']
+'Abl':['tvat', 'tvat'],['yuvābhyām','vām'],['yūṣmat', 'vaḥ', 'vo']
+'Gen':['tava', 'te'],['yuvayoḥ', 'yuvayor', 'vām'],['yūṣmākam', 'vaḥ', 'vo']
+'loc':['tvayi'],['yuvayoḥ', 'yuvayor'],['yūṣmāsu']
+}
+
+
+#====== Nouns ===========================
+
+#The first form listed in each category is the classical 'suffix', and probably the most 'frequent']
+# All n can also be 'ṇ', all m can be ṃ (this is a general orthography 'rule', not only for suffixes but for words in general)
+# The division in Gender is very artificial. It may not work with non-classical texts.
+# format: AM:{nom:['form1','form2','form3'],['dualform'],['pluralform']
+
+'AM':{
+'Nom':['aḥ','o', 'u', 'ū', 'ā', 'e', 'aṃ' ],['au'],['āh', 'ā', 'ām', 'ām', 'ān', 'āni', 'e', 'aḥ', 'o', 'āyo', 'āya']
+'Voc': ['a','ā', 'o', 'u', 'e'],['au'],['āh', 'ā', 'āho', 'āvo']
+'Acc':['am', 'u', 'a', 'o', 'e', 'ā', 'āṃ'], ['au'],['ān', āṃ. 'am', 'an', 'ā', 'ās', 'āḥ', 'a', 'e', 'i', 'u', 'āni', 'āna']
+'Inst':['ena', 'enā', 'inā', 'asā', 'ayā', 'āya', 'ā', 'a'], ['ābhyām'],['aiḥ', 'ai', 'ehi', 'ehī', 'ebhiḥ', 'ebhis', 'ebhī', 'abhiḥ', 'abhis', 'ābhi', 'ibhi']
+'Dat': ['āya', 'aya', 'aye', 'ayā'],['ābhyām'], ['ebhyaḥ']
+'Abl': ['āt', 'ā', 'a', 'āta', 'āto', 'ātu', 'ato', 'atu', 'atta', 'attaḥ', 'asmā'],['ābhyām'],[ 'ebhyaḥ']
+'Gen':['asya', 'asyā'], ['ayoḥ'], ['ānām', 'āna', 'ānam','ana', 'an', 'ān', 'ānu']
+'Loc': ['e', 'i', 'aṃhi', 'aṃṣe', 'asmin', 'asmiṃ', 'asmi', 'esmiṃ', 'esmin', 'esmi'], ['ayoḥ'], ['eṣu', 'iṣu', 'asu', 'eṣū']
+}
+'A':{Nom: ['am', 'u', 'o', 'a', 'e', 'ā', 'āṃ'], ['e'], ['āni', 'a', 'ām', 'e', 'i', 'u', 'aṃṣi', 'ānī']
+'Voc':['am'],['e'], ['āni']
+'Acc':['am', 'u', 'o', 'a', 'e', 'ā', 'āṃ'], ['e'], ['āni', 'a', 'ām', 'e', 'i', 'u', 'aṃṣi', 'ānī']
+'Inst': ['ena', 'enā', 'inā', 'asā', 'ayā', 'āya', 'ā', 'a'], ['ābhyām'],['aiḥ', 'ai', 'ehi', 'ehī', 'ebhiḥ', 'ebhis', 'ebhī', 'abhiḥ', 'abhis', 'ābhi', 'ibhi']
+'Dat': ['āya', 'aya', 'aye', 'ayā'],['ābhyām'], ['ebhyaḥ']
+'Abl': ['āt', 'ā', 'a', 'āta', 'āto', 'ātu', 'ato', 'atu', 'atta', 'attaḥ', 'asmā'],['ābhyām'],[ 'ebhyaḥ']
+'Gen':['asya', 'asyā'], ['ayoḥ'], ['ānām', 'āna', 'ānam','ana', 'an', 'ān', 'ānu']}
+'Loc': ['e', 'i', 'aṃhi', 'aṃṣe', 'asmin', 'asmiṃ', 'asmi', 'esmiṃ', 'esmin', 'esmi'], ['ayoḥ'], ['eṣu', 'iṣu', 'asu', 'eṣū']
+}
+'Ā':{
+'Nom': ['ā', 'a', 'as', 'ām', 'u', 'ī'],[ 'e'], ['āḥ', 'ā', 'a', 'āyo', 'āyā', 'āye','āvo', 'e', 'o', 'āni']
+'Voc':['e', 'i', 'a'], ['e'], ['āḥ', 'āho']
+'Acc': ['ām', 'am', 'aṃ', 'a', 'ā', 'u', 'ān', 'āram'], ['e'],['āḥ',  'ā', 'a', 'āyo', 'āyā', 'āye','āvo', 'e', 'o', 'āni','ān', 'āṃ']
+'Inst': ['ayā', 'ena', 'ayena', 'ayāye', 'āe', 'āyi', 'aye', 'āyai', 'āye', 'āyā'], ['ābhyām'], ['ābhiḥ', 'ābhi', 'āhi', 'ais', 'ebhiḥ', 'ehi']
+'Dat': ['āyai', 'āye', 'āe', 'āyi', 'aye', 'āyai'], ['ābhyām'], ['ābhyaḥ']
+'Abl': ['āyāḥ', 'ayā', 'āyām', 'āe', 'āyi', 'aye', 'āt', 'ātas', 'āto', 'āta', 'atas', 'atu'], ['ayoḥ'], ['ābhyaḥ']
+'Gen': ['āyāḥ', 'āe', 'āyi', 'aye', 'ayā', 'āyāṃ', 'asyā', 'asya'], ['ayoḥ'], ['ānām', 'āna']
+'Loc': ['āyām', 'āyaṃ', 'e', 'ayām', 'āyaṃ', 'āe', 'āyi', 'aye'], ['ayoḥ'], ['āsu', 'āsū', 'asu']
+}
+'i/īAllGender':{
+'Nom':['iḥ', 'ī', 'i', 'iṃ', 'im', 'īm', 'is','īs', 'ā']['ī', 'iṇī', 'yau', 'inau'],['ayaḥ', 'īṇi', 'inā', 'ina', 'īno', 'īna', 'īnī','yas', 'ya','yā', 'yās', 'īn', 'īṃ', 'īs', 'iyas', 'iyo', 'iya', 'iyā', 'īo', 'īyo', 'īyā', 'īya', 'īye', 'iye', 'ī', 'is', 'iḥ'] 
+'Voc':['e', 'i', 'ī'],['ī', 'iṇī', 'yau'], ['yaḥ', 'īṇi', 'ī', 'īho']
+'Acc': ['im', 'iṃ', 'i', 'īm', 'īṃ', 'y', 'ī', 'is', 'īnam', 'iya', 'iyaṃ'],['ī', 'iṇī', 'inau'],['in', 'īḥ', 'īṇi', 'inā', 'ina', 'īno', 'īna', 'īnī','yas', 'ya','yā', 'yās', 'īn', 'īṃ', 'īs', 'iyas', 'iyo', 'iya', 'iyā', 'īo', 'īyo', 'īyā', 'īya', 'īye', 'iye', 'ī', 'is', 'iḥ'] ]
+'Inst':['inā','yā', 'i', 'yā', 'ina', 'īnā', 'īye', 'īya', 'īyo', 'īyena'],['ibhyām', 'ibhyāṃ'],['ibhiḥ', 'ibḥir', 'ībhiḥ', 'ībhir', 'īhi', 'ībhis', 'ībhi', 'ihi', 'ibhi']
+'Dat':['aye', 'yai', 'iṇe', 'yai', 'ayi', 'īye','iye', 'īyai', 'ye'],['ibhyām', 'ibhyāṃ'], ['ibhyaḥ', 'ībhyaḥ','ibhyas', 'ībhyas']
+'Abl':['eḥ', 'yāḥ', 'iṇaḥ', 'ito', 'iye', 'īya', 'īyo', 'yā', 'īto'],['ibhyām', 'ibhyāṃ'], ['ibhyaḥ', 'ībhyaḥ','ibhyas', 'ībhyas','īhi']
+'Gen':['eḥ', 'yāḥ', 'iṇaḥ', 'yāḥ', 'isya', 'e', 'yus', 'inaḥ', 'īye', 'īya', 'īyu', 'iyo', 'yā', 'yas', 'aye', 'ayi', 'īyaṃ'],['yoḥ', 'yor', 'ṇoḥ'], ['īnām', 'īṇām', 'īnāṃ', 'īṇāṃ', 'inā']
+'Loc':['au', 'yām', 'yāṃ', 'iṇi', 'iṃ', 'yau', 'e', 'esmiṃ','ismiṃ', 'ismi', 'iyi', 'iye', 'iyā', 'īyo', 'ya', 'īyaṃ', 'iyaṃ'],['yoḥ', 'yor', 'ṇoḥ'], ['iṣu', 'īṣu', 'īṣū']
+}
+'u/ūAllGender':{
+'Nom':['uḥ', 'ūḥ', 'u', 'v', 'ū', 'uṃ'],['ū', 'unī','vau'], ['vaḥ', 'ūni', 'avaḥ', 'ava', 'uvaḥ', 'uno', 'ūna', 'ūni', 'uni', 'uṃ', 'um', 'ūs', 'ūḥ', 'uḥ', 'ūyo', 'uyo', 'ū', 'u']
+'Voc':['o', 'u', 'ū'], ['ū', 'unī', 'vau'], ['vaḥ', 'ūni', 'ūḥ', 'o']
+'Acc':['um', 'ūm', 'u', 'ū', 'um', 'uyam', 'unam', 'una', 'uvam', 'us'],['ū', 'vau', 'unī'],['ūḥ', 'ūn', 'ūni', 'avaḥ', 'avo', 'ava', 'uno', 'ūna', 'uni', 'ūni', 'uṃ', 'um', 'ūyo', 'uyo', 'ū', 'u']
+'Inst':['unā', 'vā', 'ūnā', 'ūye', 'uye', 'ūya', 'uya', 'ūyo', 'ūyaṃ', 'ūyam'], ['ubhyām', 'ubhyāṃ'], ['ubhiḥ', 'ubhir', 'ūbhiḥ', 'ūbhir', 'ūhi', 'ūbhi', 'uhi', 'ubhi', 'ubhīr']
+'Dat':['ave', 'vai', 'une','ūye', 'uye', 'ūya', 'uya', 'ūyo', 'ūyaṃ', 'ūyam'], ['ubhyām', 'ubhyāṃ'],['ubhyaḥ', 'ubhyas', 'ūbhyaḥ', 'ūbhyas']
+'Abl':['yoḥ', 'yor', 'oḥ', 'or', 'unaḥ', 'uno', 'ūye', 'uye', 'ūya', 'uya', 'ūyo', 'ūyaṃ', 'ūyam'], ['ubhyām', 'ubhyāṃ'], ['ubhyaḥ', 'ubhyas', 'ūbhyaḥ', 'ūbhyas']]
+'Gen':['yoḥ', 'yor', 'oḥ', 'or', 'vaḥ', 'unaḥ','uno', 'usya', 'ūsya', 'o','ūye', 'uye', 'ūya', 'uya', 'ūyo', 'ūyaṃ', 'ūyam'],['voḥ', 'unoḥ'],['unām', 'ūnāṃ', 'ūnam', 'unāṃ', 'ūna']
+'Loc':['vām', 'vāṃ', 'enau', 'au', 'uni', 'une', 'usmin', 'usmiṃ', 'ūye', 'uye', 'ūya', 'uya', 'ūyo', 'ūyaṃ', 'ūyam'],['voḥ', 'unoḥ'], ['uṣu', 'ūṣu']
+}
+'sAllGender':{
+'Nom':['ḥ','asas', 'aso', 'asā', 'asaṃ', 'asa', 'ā', 'as', 'aṃ', 'am'],['sī'], ['ṃsi', 'ūṃsi', 'āṃsi', 'ās', 'āni', 'ānī']
+'Voc':['ḥ', 'asa'],['sī'],['ṃsi', 'ūṃsi', 'āṃsi']
+'Acc':['ḥ','am', 'aṃ', 'ām'], ['sī'], ['ṃsi', 'ūṃsi', 'āṃsi', 'āni', 'ānī']
+'Inst':['sā', 'ṣā', 'ena'],['obhyām', 'obhyāṃ', 'rbhyām', 'rbhyāṃ'], ['obhiḥ', 'rbhiḥ', 'obhiṛ', 'rbhir', 'ais']
+'Dat':['se'], ['obhyām', 'obhyāṃ', 'rbhyām', 'rbhyāṃ', 'ābhyāṃ'], ['obhyaḥ', 'rbhyaḥ']
+'Abl':['saḥ', 'āto', 'ato', 'ātu', 'atu'],  ['obhyām', 'obhyāṃ', 'rbhyām', 'rbhyāṃ'], ['obhyaḥ', 'rbhyaḥ']
+'Gen':['saḥ', 'asya'], ['soḥ', 'sor'], ['sām', 'ṣām', 'ānām', 'asānām']
+'Loc':['si', 'asmi', 'ase'], ['soḥ', 'sor'], ['ḥsu', 'eṣu']
+}
+'nAllGende':{
+'Nom':['ā', 'ah','o', 'u', 'a', 'ās', 'ānu','nas'],['ānau', 'nī'], ['ānaḥ', 'āni', 'ās', 'ā', 'āna', 'nas', 'ānaḥ', 'āna']
+'Voc':['an', 'a', 'ā','e'], ['nī', 'ānau'], ['ānaḥ', 'āni']
+'Acc':['ānam', 'a', 'aṃ', 'am', 'ā', 'āna', 'anam', 'anu', 'ana'],['ānau', 'nī'], ['aḥ', 'āni', 'ā', 'ānaḥ', 'āna']
+'Inst':['ñā', 'nā','ena', 'ina', 'nena', 'anena'], ['abhyām'],['abhiḥ', 'abhir', 'ehi', 'ānais', 'ānehi', 'ābhis']
+'Dat':['ñe','ne'], ['abhyām'], ['abyaḥ']
+'Abl':['ñaḥ', 'naḥ', 'a', 'nātu'],[ 'abhyām'], ['abhyaḥ']
+'Gen':['ñāḥ', 'naḥ','asya', 'ana', 'āno', 'ānas', 'ānasya', 'nasya'], ['ñoḥ', 'noḥ'], ['ñām', 'nām', 'ānaṃ']
+'Loc':['ani', 'ni', 'ñi', 'i', 'e','ne', 'āne', 'āni'],['ñoḥ', 'noḥ'], ['asu', 'āneṣu', 'neṣu']
+}
+'atAllGender':{
+'Nom':['an', 'ān', 'nta', 'ntā', 'ntu', 'nto', 'ntaḥ', 'at', 'tas', 'ta', 'tu', 'as', 'ā'], ['ntau', 'atī'], ['ntaḥ', 'nti', 'ntās','ntā', 'nti', 'ntāni']
+'Voc':['an', 'nta', 'at', 'tā'],['ntau'], ['ntaḥ']
+'Acc':['ntam', 'at','āntam', 'stām', 'nta', 'ntaṃ', 'tam', 'tu', 'tāṃ', 'a'],['ntau', 'atī'],['taḥ', 'anti','ntān', 'ntām', 'ntā', 'ntāni', 'āni', 'a', 'ā', 'tā', tāni ]
+'Inst':['tā', 'ntena', 'ena'], ['adbhyām'], ['dhiṛ', 'dbhir', 'ntais', 'ntebhis']
+'Dat':['te'], 'adbhyām'],['adhyaḥ', 'ntebhyas']
+'Abl':['taḥ'],['adbhyām'],['adbhyaḥ', 'adbhyo']
+'Gen':['taḥ', 'to', 'ntasya', 'asya', 'tu'], ['toḥ'],['tām', 'ntānām', 'ntānam', 'ntān', 'ntān', 'tānām', 'ntam', 'tu']
+'Loc':['ti', 'āyām', 'te'], ['toḥ'], ['tsu', 'nteṣu']
+}
+'inAllGender':{
+'Nom':['ī', 'i'], ['inau', 'inī'],['inaḥ', 'īni']
+'Voc':['in', 'i'], ['inau', 'inī'], ['inaḥ', 'īni']
+'Acc':['inam', 'i'], ['inau', 'inī'], ['inaḥ', 'īni']
+'Inst':['inā'],['ibhyām'],['ibhi', 'ibhir', 'ṛhi']
+'Dat':['ine'], ['ibhyām'],['ibhyaḥ', 'ibhyo']
+'Abl':[, 'usmā'], ['ibhyām'],['ibhyaḥ', 'ibhyo']
+'Gen':['inaḥ', 'uno'], ['inoḥ'], ['inām', 'unām', 'ṛṇām']
+'Loc':['ini'], ['inoḥ'], ['iṣu', 'ṛṣu']
+}
+'ṛ':{
+'Nom':['ā', 'a', 'uḥ', 'u', 'ās'],['ārau'], ['āraḥ', 'ā']
+'Voc':['aḥ', 'ar','e'], ['ārau'], ['āraḥ']
+'Acc':['āram', 'āṃ', 'ā', 'ṛ', 'uṃ', 'u'],['ārau'],['ṛn', 'ṛm']
+'Inst':['rā', 'ṛṇā', 'unā'],['ṛbhyām'],['ṛbhiḥ', 'ṛbhir']
+'Dat':['re'],['ṛbhyām'],['ṛbhyaḥ', 'ṛbhyo']
+'Abl':['uḥ', 'ur'],['ṛbhyām'], ['ṛbhyaḥ', 'ṛbhyo']
+'Gen':['uḥ', 'ur', 'aro', 'u', 'uno'], ['oḥ', 'or'],['ṛṇām', 'unām']
+'Loc':['ari'],['oḥ', 'or'], ['ṛṣu']
+} #the preceding may apply to all genders
+'ṛ':{
+'Nom':['ṛ', 'rā', 'rī'],['ṛṇī'],['ṛṇi']
+'Voc':['ṛ', 'e'],['ṛṇī'],['ṛṇi']
+'Acc':['ṛ', 'ām'],['ṛṇī'],['ṛṇi','ṛn', 'ṛm']
+'Inst':['ṛṇā'],['ṛbhyām'],['ṛbhiḥ', 'ṛbhir', 'ṛhi']
+'Dat':['ṛṇe', 'are'], ['ṛbhyām'],['ṛbhiḥ', 'ṛbhir']
+'Abl':['ṛṇaḥ', 'ṛno'],['ṛbhyām'], ['ṛbhyaḥ', 'ṛbhyo']
+'Gen':['ṛṇaḥ', 'ṛno', 'aro', 'u'], ['ṛhoḥ'], ['ṛṇām']
+'Loc':['ṛṇi'],['ṛṇoḥ'],['ṛṣu']
+}
+
+}
+##============ Verb ========================
+
+
+#the preceding may apply to all genders# rules to generate conjugated forms from the third person singular verb stem
+# example: 
+# in the StarDict wordlist we find the form akṣati , remove ti and attach all suffixes
+
+# !!forms in 'te', e.g. ucyate are 'passive', attach ONLY the MiddlePassive suffixes below
+
+# please add to the wordlist the following forms: 'avocat', 'avocanti', 'āha', āhuḥ. they are frequently occurring 'aorists', no need to conjugate 'them', other persons do not occur.
+
+Verbal={
+#ati/otiVerb:{
+'PresentActive':{
+'1s':[ 'mi']
+'2s':[ 'si']
+'3s':['ti']
+'1d':['vaḥ', 'vas']
+'2d':['thaḥ', 'thas']
+'3d':['tas', 'taḥ']
+'1p':['mas', 'maḥ', 'āmaḥ']
+'2p':[  'tha']
+'3p':['nti', 'anti']
+}
+'PresentMiddlePassive':{
+'1s':[ 'e']
+'2s':[ 'se']
+'3s':['te']
+'1d':['vahe', 'ḥvahe']
+'2d':['ethe', 'āthe']
+'3d':['et', 'ate']
+'1p':['mahe', 'ḥmahe', 'āmahe']
+'2p':[  'dhve']
+'3p':['te', 'nte']
+}
+'ImpfActive':{  # for the imperfect put an 'a' before the stem:['e'.g. 'bhavati'> 'abhavan', when stem starts in 'vowl', sandhi applies:[  'akṣati' > ākṣan
+'1s':['m', 'am']
+'2s':[ 's']
+'3s':['t']
+'1d':[ 'va']
+'2d':[ 'tam']
+'3d':[ 'tām']
+'1p':['ma', 'āma']
+'2p':[  'ta']
+'3p':['n', 'an', 'ur', 'uḥ']
+}
+'ImpfMiddlePassive':{  # for the imperfect put an 'a' before the stem:['e'.g. 'bhavati'> 'abhavan', when stem starts in 'vowl', sandhi applies:[  'akṣati' > ākṣan
+'1s':[ 'i']
+'2s':['thās', 'thāḥ']
+'3s':['ta']
+'1d':[ 'vahi']
+'2d':['ethām', 'āthām']
+'3d':['etām', 'ātam']
+'1p':['mahi', 'āmahi']
+'2p':[  'dhvam']
+'3p':['nta', 'ata']
+}
+'OptativeActive':{
+'1s':['eyam', 'yām']
+'2s':['es', 'yās']
+'3s':['et', 'yāt']
+'1d':['eva', 'yāva']
+'2d':['etam', 'yātam']
+'3d':['etām', 'yātām']
+'1p':['emam' 'yāma']
+'2p':['eha', 'yāta']
+'3p':['eyur', 'eyuḥ', 'yur', 'yuḥ']
+}
+'OptativeMiddlePassive':{ 
+'1s':['eya', 'īya']
+'2s':['ethās', 'ethāḥ', 'īthās']
+'3s':['eta', 'īta']
+'1d':['evahi', 'īvahi']
+'2d':['eyāthām', 'īyāthām']
+'3d':['eyātam', 'īyātām']
+'1p':['emahi', 'īmahi']
+'2p':['dhvam', 'īdhvam']
+'3p':['eran', 'īran']
+}
+'ImperativeActiv':{
+
+'1s':[ 'āni']
+'2s':['dhi', 'hi']
+'3s':['tu']
+'1d':['āva']
+'2d':['tam']
+'3d':[ 'tām']
+'1p':[ 'āma']
+'2p':[ 'ta']
+'3p':['ntu', 'antu']
+}
+'ImperativeMiddlepassiv':{
+
+'1s':[ 'ai']
+'2s':[ 'sva']
+'3s':['tām']
+'1d':['āvahai']
+'2d':['ethām', 'ātham']
+'3d':['ethām', 'ātām']
+'1p':[ 'āmahai']
+'2p':['dhvam']
+'3p':['ntām', 'atām']
+}
+
+
+}
+
+
+ 
